86cb845131b81fa645dc319a727d7a9c1f274024
Vladimir Grigore <vladimir.grigore@gmail.com>
2017-03-13 21:46:35 +0000

    Call getRepoContributors method with hard-coded values

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,12 @@
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  // TODO implement the function
+}
+
+getRepoContributors('jquery', 'jquery', function(err, result){
+  console.log("Errors:", err);
+  console.log("Result:", result);
+})

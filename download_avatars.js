1b760dc8debe6dab168710343ed49507bf13a3f6
Vladimir Grigore <vladimir.grigore@gmail.com>
2017-03-14 00:06:06 +0000

    Add comments

--- download_avatars.js
+++ download_avatars.js
@@ -4,15 +4,17 @@ const GITHUB_TOKEN = '91b2f0714f02e3d8c25476ded3ba6764e53fbdc1';
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+//Build a custom API request url
 function buildRequestURL(repoOwner, repoName){
   var requestURL = `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`;
   return requestURL;
 }
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // TODO implement the function
+  //build the request url
   let queryUrl = buildRequestURL(repoOwner, repoName);
 
+  //construct an option object to hold our request url and user-agent
   var options = {
     url: queryUrl,
     headers: {
@@ -20,12 +22,13 @@ function getRepoContributors(repoOwner, repoName, cb) {
     }
   };
 
+  //Make the request and return the body
   request(options, (err, response, body) => {
     console.log(body);
   });
 }
 
-
+//Call the getRepoContributors function and handle error
 getRepoContributors('jquery', 'jquery', function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
